03cd946d82f5bfd04956b3c3a8820e230f816afb
Erdem Yüksel <erdemyuksel04@gmail.com>
2026-09-24 22:44:29 +0300

    chore: release v0.1.16

--- apps/updater/obj/Release/net9.0-windows/win-x64/EchoUpdater.AssemblyInfo.cs
+++ apps/updater/obj/Release/net9.0-windows/win-x64/EchoUpdater.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Echo Windows Updater")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144143d2da04de792de3c15d6b961c016b716352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b77bb8c1a9910a7784c65368461c8dc9c0b2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("EchoUpdater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EchoUpdater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
